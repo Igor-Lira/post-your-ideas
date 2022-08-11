47a362e97d001a534bac45da3390a9bcb00615c8
Igor Lira <igorlirap@gmail.com>
2022-08-10 22:31:53 -0300

    Create header, List Components and logic to add new post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'mean-course';
+  storedPosts = [] as any;
+  onPostAdded(post: any) {
+    this.storedPosts.push(post);
+  }
 }
