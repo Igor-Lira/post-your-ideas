cde7e8efe7d3ee101af4cefe5ceb0fda8e62a63b
Igor Lira <igorlirap@gmail.com>
2022-08-10 23:24:07 -0300

    Use Observables instead of events

--- src/app/post/post-list/post-list-component.ts
+++ src/app/post/post-list/post-list-component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from '../post-module';
+import { PostService } from '../post-service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list-component.html',
   styleUrls: ['./post-list-component.css'],
 })
-export class PostListComponent {
-  @Input() posts: Post[] = [];
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSubs: Subscription;
+  constructor(public postsService: PostService) {
+    this.postsSubs = this.postsService
+      .getPostUpdatedListener()
+      .subscribe((posts: Post[]) => (this.posts = posts));
+  }
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+  }
+  ngOnDestroy(): void {
+    this.postsSubs.unsubscribe();
+  }
 }
