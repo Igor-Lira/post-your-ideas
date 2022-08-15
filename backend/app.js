d9d6b5ce6928f24e7177a169b79efc90ed2b9c49
Igor Lira <igorlirap@gmail.com>
2022-08-15 07:19:32 -0300

    Organize backend routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const postsRoutes = require("./routes/post");
 
 const app = express();
 
@@ -10,8 +11,6 @@ mongoose
   )
   .then(() => console.log("Connected to database!"))
   .catch(() => console.log("Not connected"));
-
-const Post = require("./models/post");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,54 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((createdPost) => {
-    res
-      .status(201)
-      .json({ message: "Post added successfully!", id: createdPost._id });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents,
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found" });
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then(() => {
-    res.status(200).json({ message: "Updated successfull!" });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 app.set("port", 3000);
 
