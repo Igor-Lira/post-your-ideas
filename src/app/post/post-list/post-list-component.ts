c583fc22a5ee9db0da988c2bf237dd2d55fcbbdc
Igor Lira <igorlirap@gmail.com>
2022-08-28 16:51:43 -0300

    Pagination in front with Paginator Module

--- src/app/post/post-list/post-list-component.ts
+++ src/app/post/post-list/post-list-component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Subscription } from 'rxjs';
 import { Post } from '../post-module';
 import { PostService } from '../post-service';
@@ -11,6 +12,10 @@ import { PostService } from '../post-service';
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 5;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSubs: Subscription;
   constructor(public postsService: PostService) {
     this.isLoading = true;
@@ -22,12 +27,17 @@ export class PostListComponent implements OnInit, OnDestroy {
       });
   }
   ngOnInit(): void {
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
   ngOnDestroy(): void {
     this.postsSubs.unsubscribe();
   }
   onDelete(postId: string) {
     this.postsService.deletePost(postId);
+  }
+  onChangedPage(pageData: PageEvent) {
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 }
