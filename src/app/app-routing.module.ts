8555fd72b66beae90f1060de92ffdb5dbda6a670
Igor Lira <igorlirap@gmail.com>
2022-09-11 18:55:14 -0300

    add Auth Gard to create and edit routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/atuh-gard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/signup/signup.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
@@ -7,13 +8,18 @@ import { PostListComponent } from './post/post-list/post-list-component';
 
 const routers: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  {
+    path: 'edit/:postId',
+    component: PostCreateComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routers)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
