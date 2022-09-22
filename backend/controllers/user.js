f413ad97467729b00862eb97cee090ed53c3d837
Igor Lira <igorlirap@gmail.com>
2022-09-21 21:31:46 -0300

    use controllers

---
+++ backend/controllers/user.js
@@ -0,0 +1,61 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "User created!",
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Invalid authentication credentials!",
+        });
+      });
+  });
+};
+
+exports.userLogin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Invalid authentication credentials!" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) {
+      return res
+        .status(401)
+        .json({ message: "Invalid authentication credentials!" });
+    }
+    const token = jwt.sign(
+      { email: user.email, userId: user._id },
+      "secrete_this_should_be_longer",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      token,
+      expiresIn: 3600,
+      userId: user._id,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid authentication credentials!",
+      err,
+    });
+  }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,61 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
-router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-    });
-    user
-      .save()
-      .then((result) => {
-        res.status(201).json({
-          message: "User created!",
-          result: result,
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "Invalid authentication credentials!",
-        });
-      });
-  });
-});
+const UserController = require("../controllers/user");
 
-router.post("/login", async (req, res, next) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(401).json({ message: "Invalid authentication credentials!" });
-    }
-    const isPasswordCorrect = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!isPasswordCorrect) {
-      return res.status(401).json({ message: "Invalid authentication credentials!" });
-    }
-    const token = jwt.sign(
-      { email: user.email, userId: user._id },
-      "secrete_this_should_be_longer",
-      { expiresIn: "1h" }
-    );
+router.post("/signup", UserController.createUser);
 
-    res.status(200).json({
-      token,
-      expiresIn: 3600,
-      userId: user._id,
-    });
-  } catch (err) {
-    return res.status(401).json({
-      message: "Invalid authentication credentials!",
-      err,
-    });
-  }
-});
+router.post("/login", UserController.userLogin);
 
 module.exports = router;
